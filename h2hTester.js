aacb22980d1a5e52ec8e5384370b839886cbdcd4
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2024-03-05 18:24:59 +0200

    sftp shenanigans

--- h2hTester.js
+++ h2hTester.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const {NodeSSH} = require('node-ssh')
 const dotenv = require('dotenv')
+const Client = require('ssh2-sftp-client');
+const sftp = new Client();
+
 
 dotenv.config()
 
@@ -9,16 +12,16 @@ const ssh = new NodeSSH()
 const passphrase = process.env.PASSPHRASE;
 const homePath = process.env.HOME_PATH
 
-try { 
-  ssh.connect({
-    host: 'github.com',
-    username: 'git',
-    privateKeyPath: `${homePath}.ssh/id_ed25519`,
-    passphrase
 
-  }).then(function(status) {
-    console.log(status)
-  })
-} catch(exception) {
-  console.log(exception)
-}
+sftp.connect({
+  host: '127.0.0.1',
+  port: '2022',
+  username: 'yonatan7',
+  password: 'xknbsrv516'
+}).then(() => {
+  return sftp.list('/Documents/Elementor/Mesh');
+}).then(data => {
+  return sftp.get(`/Documents/Elementor/Mesh/${data[0].name}`, '/home/../Users/yonatanboehm/newfile.txt')
+}).then(() => {
+  sftp.end();
+})

b4d4eae0435eae25588641ee036fc7c13f77224c
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2024-03-10 11:47:45 +0200

    created CLI connection file that uses SFTP connection test module

--- h2hTester.js
+++ h2hTester.js
@@ -1,30 +1,22 @@
-const dotenv = require('dotenv')
 const Client = require('ssh2-sftp-client');
-const fs = require('node:fs/promises')
-
-// open sftpGo server
-
-dotenv.config()
+const fs = require('fs')
 
 const sftp = new Client();
 
-// const passphrase = process.env.PASSPHRASE
+const sftpTestConnection = (configObject) => {
+  sftp.connect(configObject)
+  .then(() => {
+    return sftp.exists('/')
+  })
+  .then(data => {
+    console.log(data)
+    console.log('Successful connection')
+  }).then(() => {
+    sftp.end();
+  }).catch(err => {
+    console.error(err.message)
+    sftp.end();
+  })
+}
 
-const agent = process.env.SSH_AUTH_SOCK
-
-const fileToGet = Math.floor(Math.random() * 2 + 1)
-
-sftp.connect({
-  host: '127.0.0.1',
-  port: '2022',
-  username: 'yonatan7',
-  // privateKey: fs.readFileSync('/home/../Users/yonatanboehm/.ssh/id_ed25519'),
-  // passphrase,
-  agent,
-  agentForward: true
-  
-}).then(() => {
-  return sftp.fastGet(`/Documents/file${fileToGet}.txt`, '/home/../Users/yonatanboehm/newfile.txt')
-}).then(() => {
-  sftp.end();
-})
+module.exports = { sftpTestConnection }

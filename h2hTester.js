f1dd1e2b26e9516f4d878530cd9a347d3c2e0cc6
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2024-03-10 11:50:28 +0200

    added function output - connection status

--- h2hTester.js
+++ h2hTester.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const sftp = new Client();
 
 const sftpTestConnection = (configObject) => {
+  let connection
   sftp.connect(configObject)
   .then(() => {
     return sftp.exists('/')
@@ -11,12 +12,15 @@ const sftpTestConnection = (configObject) => {
   .then(data => {
     console.log(data)
     console.log('Successful connection')
+    connection = true
   }).then(() => {
     sftp.end();
   }).catch(err => {
     console.error(err.message)
+    connection = false
     sftp.end();
   })
+  return connection
 }
 
 module.exports = { sftpTestConnection }
